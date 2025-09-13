fd07a75ce698fdfcd89902f88ee47e0ac31be526
Sijie Fan <sfan0043@student.monash.edu>
2025-09-13 15:02:42 +1000

    Configure security headers and secure storage of Firebase API keys

--- src/firebase/init.js
+++ src/firebase/init.js
@@ -8,13 +8,13 @@ import { getFirestore } from 'firebase/firestore'
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCIkw3ED6Ow7LyHhls-Rpjm-vNsQJq99eA",
-  authDomain: "fit5032-ad9a9.firebaseapp.com",
-  projectId: "fit5032-ad9a9",
-  storageBucket: "fit5032-ad9a9.firebasestorage.app",
-  messagingSenderId: "476518862976",
-  appId: "1:476518862976:web:6c4ea367b9cf11c710d665",
-  measurementId: "G-F9PQMXKCV6"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
